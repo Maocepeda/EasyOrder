4a90571a5b36639d4e8431d33e5e2ae222057282
Mauricio Morales <morales7mauro@gmail.com>
2021-11-28 17:49:49 -0500

    Registro de Usuarios

---
+++ app/src/main/java/com/mintic2022/easyorder/view/ui/activities/SignUp.kt
@@ -0,0 +1,10 @@
+package com.mintic2022.easyorder.view.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
